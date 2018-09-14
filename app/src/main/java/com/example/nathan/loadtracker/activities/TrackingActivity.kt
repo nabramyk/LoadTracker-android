e849e1fe1cd17234d70c003ec50fc420c7befbce
Nathan Abramyk <nathan.abramyk@pushinteractions.com>
2018-09-14 11:39:49 -0600

    Adding dynamic menu items.

--- app/src/main/java/com/example/nathan/loadtracker/activities/TrackingActivity.kt
+++ app/src/main/java/com/example/nathan/loadtracker/activities/TrackingActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -46,9 +48,6 @@ class TrackingActivity : AppCompatActivity() {
             driverNameInput.setText(sharedPrefs.getString("name", ""))
             companyNameInput.setText(sharedPrefs.getString("company", ""))
         }
-
-        //history_breakdown.text = updateTotalLoadsTracked()
-        //average_load_time.text = updateAverageRunTime()
     }
 
     override fun onNewIntent(intent: Intent) {
@@ -94,21 +93,19 @@ class TrackingActivity : AppCompatActivity() {
 
         Snackbar.make(view, "Tracked!", Snackbar.LENGTH_LONG).show()
 
-        //history_breakdown.text = updateTotalLoadsTracked()
-        //average_load_time.text = updateAverageRunTime()
+        updateTotalLoadsTracked()
+        updateAverageRunTime()
 
     }
 
-    private fun updateTotalLoadsTracked(): String {
-        //val loads = db.getAllLoadsForSession(title.toString())
+    private fun updateTotalLoadsTracked() {
+        val loads = database.getLoadsForSession(sessionTitle)
         val materials = HashMap<String, Int>()
 
-//        for (l in loads) {
-//            if (!materials.containsKey(l.material))
-//                materials[l.material] = 1
-//            else if (materials.containsKey(l.material))
-//                materials[l.material] = materials[l.material]!! + 1
-//        }
+        for (l in loads) {
+            if (!materials.containsKey(l.material)) materials[l.material!!] = 1
+            else if (materials.containsKey(l.material)) materials[l.material!!] = materials[l.material!!]!! + 1
+        }
 
         var formattedOutput = materials.toString()
         formattedOutput = formattedOutput.replace("=", ": ")
@@ -116,35 +113,35 @@ class TrackingActivity : AppCompatActivity() {
         formattedOutput = formattedOutput.replace("{", "")
         formattedOutput = formattedOutput.replace("}", "")
 
-        return formattedOutput
+        nav_view.menu.add(formattedOutput)
     }
 
-    private fun updateAverageRunTime(): String {
-        //val loads = db.getAllLoadsForSession(title.toString())
-//        if (loads.size == 0) {
-//            return "00:00:00.000"
-//        }
+    private fun updateAverageRunTime() {
+        val loads = database.getLoadsForSession(title.toString())
+        if (loads.isEmpty()) {
+            nav_view.menu.add("00:00:00.000")
+            return
+        }
         var hours = 0
         var minutes = 0
         var seconds = 0
         var milliseconds = 0
-//        for (l in loads) {
-//            var timeLoaded = l.timeLoaded
-//            timeLoaded = timeLoaded.replace(":", " ")
-//            timeLoaded = timeLoaded.replace(".", " ")
-//            Log.d("Time Loaded: ", timeLoaded)
-//            val components = timeLoaded.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-//            hours += Integer.parseInt(components[0])
-//            minutes += Integer.parseInt(components[1])
-//            seconds += Integer.parseInt(components[2])
-//            milliseconds += Integer.parseInt(components[3])
-//        }
-//        hours /= loads.size
-//        minutes /= loads.size
-//        seconds /= loads.size
-//        milliseconds /= loads.size
+        for (l in loads) {
+            var timeLoaded = l.timeLoaded
+            timeLoaded = timeLoaded!!.replace(":", " ")
+            timeLoaded = timeLoaded.replace(".", " ")
+            val components = timeLoaded.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            hours += Integer.parseInt(components[0])
+            minutes += Integer.parseInt(components[1])
+            seconds += Integer.parseInt(components[2])
+            milliseconds += Integer.parseInt(components[3])
+        }
+        hours /= loads.size
+        minutes /= loads.size
+        seconds /= loads.size
+        milliseconds /= loads.size
 
-        return hours.toString() + ":" + minutes + ":" + seconds + "." + milliseconds
+        nav_view.menu.add(hours.toString() + ":" + minutes + ":" + seconds + "." + milliseconds)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -156,8 +153,8 @@ class TrackingActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.view_load_history -> {
-                viewLoadTrackingHistory()
-                super.onOptionsItemSelected(item)
+                tracking_drawer_layout.openDrawer(GravityCompat.END)
+                false
             }
             else -> super.onOptionsItemSelected(item)
         }
