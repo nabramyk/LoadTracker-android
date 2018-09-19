0cd193d0efec3f30d95da741c8a162583f1b1474
Nathan Abramyk <nathan.abramyk@pushinteractions.com>
2018-09-19 16:04:32 -0600

    Adding a row number for each load.

--- app/src/main/java/com/example/nathan/loadtracker/arrayadapters/TrackingHistoryAdapter.kt
+++ app/src/main/java/com/example/nathan/loadtracker/arrayadapters/TrackingHistoryAdapter.kt
@@ -31,6 +31,7 @@ class TrackingHistoryAdapter(context: Context,
     inner class LoadViewHolder(parent: ViewGroup)
         : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.cell_load, parent, false)) {
         fun bindViewHolder(load: Load) {
+            itemView.tvCounter.text = load.id.toString()
             itemView.tvUnitId.text = "Unit ID: " + load.unitId
             itemView.tvMaterial.text = "Material: " + load.material
             itemView.tvTimeLoaded.text = "Time Loaded: " + load.timeLoaded
