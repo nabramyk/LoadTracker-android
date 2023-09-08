1a1f56dbbadc1168d9c3680f65fbbafd7ac3ce16
Nathan <nabramyk@gmail.com>
2023-09-07 19:03:20 -0600

    Updated dependencies.

--- app/src/main/java/com/example/nathan/loadtracker/core/database/LoadTrackerDatabase.kt
+++ app/src/main/java/com/example/nathan/loadtracker/core/database/LoadTrackerDatabase.kt
@@ -8,14 +8,11 @@ import com.example.nathan.loadtracker.core.database.entities.MyObjectBox
 import io.objectbox.Box
 import io.objectbox.BoxStore
 import io.objectbox.kotlin.boxFor
-import io.objectbox.query.Query
-import io.objectbox.query.QueryBuilder
 
 object LoadTrackerDatabase {
 
     private lateinit var boxStore: BoxStore
 
-    private val loadBox: Box<Load> by lazy { boxStore.boxFor() }
     private val jobSessionBox: Box<JobSession> by lazy { boxStore.boxFor() }
 
     fun init(context: Context) {
@@ -27,9 +24,7 @@ object LoadTrackerDatabase {
     fun getJobSessions(): List<JobSession> = jobSessionBox.all
     fun getJobSession(title: String): JobSession = jobSessionBox.query(JobSession_.jobTitle.equal(title)).build().findFirst() ?: JobSession()
     fun getLoadsForSession(jobTitle: String): List<Load> = jobSessionBox.query(JobSession_.jobTitle.equal(jobTitle)).build().findFirst()?.loads?.toList() ?: emptyList()
-    fun deleteJobSession() {
 
-    }
 
     fun addJobSession(title: String) = jobSessionBox.put(JobSession(jobTitle = title))
 }
